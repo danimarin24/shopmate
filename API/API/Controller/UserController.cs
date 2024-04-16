cbf39359a1f8d9e8b46202676577f2d2bdb25042
Daniel Marín <daniel_marinbocanegra@iescarlesvallbona.cat>
2024-04-16 19:51:43 +0200

    WIP googleToken and facebookToken

--- API/API/Controller/UserController.cs
+++ API/API/Controller/UserController.cs
@@ -55,6 +55,34 @@ namespace API.Controller
 
             return user;
         }
+        
+        // GET: api/User/google/53412efadsfa582134e5sfads87f5dasf4asdf8asdfasdf5asf5a4sdf5asdf5asdf52c4xz81234r6fardfa2f3ad5sfa6sdfas9fv5c8127425daf4as2xc.....
+        [HttpGet("google/{token}")]
+        public async Task<ActionResult<User>> GetGoogleUser(string token)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.GoogleToken.Equals(token));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+        
+        // GET: api/User/facebook/53412efadsfa582134e5sfads87f5dasf4asdf8asdfasdf5asf5a4sdf5asdf5asdf52c4xz81234r6fardfa2f3ad5sfa6sdfas9fv5c8127425daf4as2xc.....
+        [HttpGet("facebook/{token}")]
+        public async Task<ActionResult<User>> GetFacebookUser(string token)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.FacebookToken.Equals(token));
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
 
         // PUT: api/User/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a1ea701cb8b8d066a89109b4f2113ab7f7cd361")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee218e4515e58da13b7e1f4df6c1e4951a18f1d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
