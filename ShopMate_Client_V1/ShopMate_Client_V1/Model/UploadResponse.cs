053b1d3c442145e2e75ac916e5fdea1164f5c7a2
DESKTOP-K3M32MF\cv <martin_gonzalezfernandez@iescarlesvallbona.cat>
2024-05-14 19:19:30 +0200

    New class to read the JSON from API response POST Image

---
+++ ShopMate_Client_V1/ShopMate_Client_V1/Model/UploadResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopMate_Client_V1.Model
+{
+    public class UploadResponse
+    {
+        [JsonProperty("finalUrl")]
+        public string FinalUrl { get; set; }
+    }
+}
