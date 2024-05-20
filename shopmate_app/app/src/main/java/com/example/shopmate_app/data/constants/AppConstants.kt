eb05e916008d8f42634d770fb972d550fc0882d9
Daniel Marín <daniel_marinbocanegra@iescarlesvallbona.cat>
2024-05-20 19:11:09 +0200

    Remove static ip config, add ngrok config

--- shopmate_app/app/src/main/java/com/example/shopmate_app/data/constants/AppConstants.kt
+++ shopmate_app/app/src/main/java/com/example/shopmate_app/data/constants/AppConstants.kt
@@ -3,7 +3,8 @@ package com.example.shopmate_app.data.constants
 object AppConstants {
     const val BASE_API_URL_IP = "http://172.16.24.21"
     const val BASE_API_URL_PORT = "6385"
-    const val BASE_API_URL = "${BASE_API_URL_IP}:${BASE_API_URL_PORT}/"
+    //const val BASE_API_URL = "${BASE_API_URL_IP}:${BASE_API_URL_PORT}/"
+    const val BASE_API_URL = "https://boa-possible-mudfish.ngrok-free.app/" // using ngrok
     const val API_KEY = "c11cddd5b1554b78b6532b41287bd243"
     const val GOOGLE_ID_TOKEN = "544701638538-ccp0cp41t4r10ofpl8biku4ckh457hm8.apps.googleusercontent.com"
 
