d962a198bd74e43dd93e8910a5579598517d1089
Martin_Gonzalez <martin_gonzalezfernandez@iescarlesvallbona.cat>
2024-05-21 19:29:49 +0200

    WIP Edit user profile setting

--- shopmate_app/app/src/main/java/com/example/shopmate_app/ui/fragments/profile/ProfileSettingFragment.kt
+++ shopmate_app/app/src/main/java/com/example/shopmate_app/ui/fragments/profile/ProfileSettingFragment.kt
@@ -12,6 +12,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Spinner
 import androidx.activity.viewModels
@@ -22,10 +23,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.shopmate_app.R
 import com.example.shopmate_app.databinding.FragmentProfileBinding
 import com.example.shopmate_app.databinding.FragmentProfileSettingBinding
+import com.example.shopmate_app.domain.entities.newtworkEntities.BoardEntity
 import com.example.shopmate_app.ui.activities.LoginActivity
 import com.example.shopmate_app.ui.adapters.ColorsChoseAdapter
 import com.example.shopmate_app.ui.viewmodels.MainViewModel
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.google.android.material.textview.MaterialTextView
@@ -50,11 +53,39 @@ class ProfileSettingFragment : Fragment() {
         binding.txtLogout.setOnClickListener {
             showLogoutDialog()
         }
-
-
-
-
+        binding.txtProfile.setOnClickListener {
+            showEditProfile()
+        }
         return binding.root
+    }
+
+    fun showEditProfile() {
+        val dialog = Dialog(findNavController().context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_profile_edit_dialog)
+
+        val ivProfilePicker = dialog.findViewById<ImageView>(R.id.ivProfilePicker)
+        val etPassword = dialog.findViewById<TextInputEditText>(R.id.etPassword)
+        val etUsername = dialog.findViewById<TextInputEditText>(R.id.etUsername)
+        val etName = dialog.findViewById<TextInputEditText>(R.id.etName)
+        val btnSave = dialog.findViewById<Button>(R.id.btnSave)
+        val ivClose = dialog.findViewById<ImageView>(R.id.ivClose)
+
+        ivClose.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        btnSave.setOnClickListener {
+
+        }
+
+
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
     }
 
     private fun showLogoutDialog() {
