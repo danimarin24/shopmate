c9fcdecd493c5de8e1e6c995d9507f68d4618ec2
Martin_Gonzalez <martin_gonzalezfernandez@iescarlesvallbona.cat>
2024-05-21 16:07:05 +0200

    Fix UserRepository to register

--- shopmate_app/app/src/main/java/com/example/shopmate_app/domain/repo/UserRepository.kt
+++ shopmate_app/app/src/main/java/com/example/shopmate_app/domain/repo/UserRepository.kt
@@ -18,49 +18,49 @@ class UserRepository @Inject constructor(private val api: UserService){
     suspend fun getUserById(id: Int): UserEntity? {
         val response = api.getUserById(id)
         UserProvider.user = response
-        return response!!
+        return response
     }
 
     suspend fun getUserStats(id: Int): UserStatsEntity? {
         val response = api.getUserStats(id)
         UserStatsProvider.stats = response
-        return response!!
+        return response
     }
 
     suspend fun getUserByEmail(email: String): UserEntity? {
         val response = api.getUserByEmail(email)
         UserProvider.user = response
-        return response!!
+        return response
     }
 
     suspend fun getUserByUsername(username: String): UserEntity? {
         val response = api.getUserByUsername(username)
         UserProvider.user = response
-        return response!!
+        return response
     }
 
     suspend fun getUserByGoogleSub(googleSub: String): UserEntity? {
         val response = api.getUserByGoogleSub(googleSub)
         UserProvider.user = response
-        return response!!
+        return response
     }
 
     suspend fun getGoogleSubByGoogleToken(googleToken: String): String? {
         val response = api.checkGoogleToken(googleToken)
         UserProvider.googleSub = response
-        return response!!
+        return response
     }
 
     suspend fun getUsernameGenerated(name: String): String? {
         val response = api.getUsernameGenerated(name)
         UserProvider.usernameGenerated = response
-        return response!!
+        return response
     }
 
     suspend fun addUser(user: UserEntity): UserEntity? {
         val response = api.addUser(user)
         UserProvider.user = response
-        return response!!
+        return response
     }
 
 }
