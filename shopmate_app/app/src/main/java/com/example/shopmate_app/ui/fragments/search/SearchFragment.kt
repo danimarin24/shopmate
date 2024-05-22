7a3ec00a3e20ca8162bca7a0d4e1b52fe5ebff5f
Martin_Gonzalez <martin_gonzalezfernandez@iescarlesvallbona.cat>
2024-05-22 19:07:56 +0200

    Add setupObservers to SearchFragment

--- shopmate_app/app/src/main/java/com/example/shopmate_app/ui/fragments/search/SearchFragment.kt
+++ shopmate_app/app/src/main/java/com/example/shopmate_app/ui/fragments/search/SearchFragment.kt
@@ -8,11 +8,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.shopmate_app.R
 import com.example.shopmate_app.databinding.FragmentSearchBinding
+import com.example.shopmate_app.ui.adapters.BoardAdapter
+import com.example.shopmate_app.ui.viewmodels.BoardViewModel
 import com.example.shopmate_app.ui.viewmodels.MainViewModel
 import com.example.shopmate_app.ui.viewmodels.ProfileViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     private val mainViewModel: MainViewModel by viewModels()
@@ -20,10 +25,12 @@ class SearchFragment : Fragment() {
 
     private lateinit var context : Context
 
+    private val boardViewModel: BoardViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflar el layout para este fragmento
         binding = FragmentSearchBinding.inflate(inflater, container, false)
         val checkBoard = binding.checkboxBoard
@@ -33,6 +40,7 @@ class SearchFragment : Fragment() {
 
         context = requireContext()
 
+        setupObservers()
         setupInitialColors()
 
         checkBoard.setOnClickListener {
@@ -83,5 +91,33 @@ class SearchFragment : Fragment() {
     private fun resetCheckBoxState(checkBox: CheckBox, underline: View, color: Int) {
         checkBox.setTextColor(resources.getColor(color))
         underline.setBackgroundColor(resources.getColor(color))
+    }
+
+    private fun setupObservers() {
+        binding.rcvSearch.showEmptyView()
+
+        boardViewModel.boards.observe(viewLifecycleOwner) { boards ->
+            if (boards.isNullOrEmpty()) {
+                binding.rcvSearch.showEmptyView()
+            } else {
+                binding.rcvSearch.hideAllViews()
+                boardViewModel.cardsByBoard.observe(viewLifecycleOwner) { cardsByBoard ->
+                    binding.rcvSearch.recyclerView.adapter = BoardAdapter(boards, cardsByBoard)
+                    binding.rcvSearch.recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                }
+            }
+        }
+
+        boardViewModel.isLoading.observe(viewLifecycleOwner) { isLoading ->
+            if (isLoading) {
+                binding.rcvSearch.showLoadingView()
+            }
+        }
+
+        boardViewModel.isError.observe(viewLifecycleOwner) { isError ->
+            if (isError) {
+                binding.rcvSearch.showErrorView()
+            }
+        }
     }
 }
