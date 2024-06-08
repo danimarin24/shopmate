26f9809f3c055e2d4515352ede2bd6797b26029c
Daniel Marín <daniel_marinbocanegra@iescarlesvallbona.cat>
2024-06-08 19:39:44 +0200

    Change all shapableimages with centerCrop, and add manual members in every list

--- shopmate_app/app/src/main/java/com/example/shopmate_app/ui/adapters/CardAdapter.kt
+++ shopmate_app/app/src/main/java/com/example/shopmate_app/ui/adapters/CardAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.shopmate_app.ui.adapters
 
+import android.content.res.Resources
 import android.graphics.Color
 import android.util.Log
 import android.util.TypedValue
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginEnd
 import androidx.fragment.app.findFragment
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
@@ -19,6 +21,9 @@ import com.example.shopmate_app.domain.entities.enums.CardViewTypeEnum
 import com.example.shopmate_app.domain.entities.newtworkEntities.CardEntity
 import com.example.shopmate_app.domain.entities.providers.CardProvider
 import com.example.shopmate_app.ui.viewmodels.CardViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.google.android.material.textview.MaterialTextView
 
 class CardAdapter(private var cardList: List<CardEntity>,
@@ -26,22 +31,31 @@ class CardAdapter(private var cardList: List<CardEntity>,
                   private val profileUserId: String,
                   private val isEditMode: Boolean,
                   private val isHome: Boolean,
-                  private val icons: List<String> = emptyList()
+                  private val icons: List<String> = emptyList(),
+                  private var members: List<String>? = emptyList()
     )
     : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
 
     private var cardSeleccionada: Int = -1
 
     class CardViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val txtCardTitle: MaterialTextView = view.findViewById(R.id.txtCardTitle)
-        //val categoryIconsContainer: LinearLayout = view.findViewById(R.id.categoryIconsContainer)
         val cardBakground: ConstraintLayout = view.findViewById(R.id.cardContraintBackground)
+        val txtCardTitle: MaterialTextView = view.findViewById(R.id.txtCardTitle)
+        val txtNSaves: MaterialTextView = view.findViewById(R.id.txtNSaves)
+        val categoryIconsContainer: LinearLayout = view.findViewById(R.id.categoryIconsContainer)
+        val profileImageCreator: ShapeableImageView = view.findViewById(R.id.profileImage)
+        val peopleProfilesContainer: LinearLayout = view.findViewById(R.id.peopleProfilesContainer)
+        val btnAddNewMember: FloatingActionButton = view.findViewById(R.id.btnAddNewMember)
+        val btnViewList: LinearLayout = view.findViewById(R.id.btnViewList)
+        val btnOpenCard: ImageView = view.findViewById(R.id.btnOpenCard)
+        val btnShareList: LinearLayout = view.findViewById(R.id.btnShareList)
+        val btnEditList: LinearLayout = view.findViewById(R.id.btnEditList)
+        val btnSaveList: LinearLayout = view.findViewById(R.id.btnSaveList)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardAdapter.CardViewHolder {
         var layoutInflater = LayoutInflater.from(parent.context)
-        //val layout = R.layout.card_item_rcv
-
 
         val layout = when (CardViewTypeEnum.entries[viewType]) {
             CardViewTypeEnum.HOME -> R.layout.card_item_view_home
@@ -81,9 +95,35 @@ class CardAdapter(private var cardList: List<CardEntity>,
             notifyDataSetChanged()
         }
 
+        holder.btnViewList.setOnClickListener {
+            if (cardSeleccionada == holder.adapterPosition) {
+                cardSeleccionada = -1
+                //changeVisibility(holder, true)
+            } else {
+                cardSeleccionada = holder.adapterPosition
+                //changeVisibility(holder, false)
+                CardProvider.selectedCard = card
+                holder.view.findNavController().navigate(R.id.cardDetailsViewFragment)
+            }
+            notifyDataSetChanged()
+        }
 
-        val sizeInDp = 20 // Tamaño en dp
-        val sizeInPx = TypedValue.applyDimension(
+        holder.btnOpenCard.setOnClickListener {
+            if (cardSeleccionada == holder.adapterPosition) {
+                cardSeleccionada = -1
+                //changeVisibility(holder, true)
+            } else {
+                cardSeleccionada = holder.adapterPosition
+                //changeVisibility(holder, false)
+                CardProvider.selectedCard = card
+                holder.view.findNavController().navigate(R.id.cardDetailsViewFragment)
+            }
+            notifyDataSetChanged()
+        }
+
+
+        var sizeInDp = 20 // Tamaño en dp
+        var sizeInPx = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP, sizeInDp.toFloat(), holder.view.resources.displayMetrics).toInt()
         if (icons.isNotEmpty()) {
             for (url in icons) {
@@ -96,7 +136,37 @@ class CardAdapter(private var cardList: List<CardEntity>,
                     .load("${AppConstants.BASE_API_URL}${url}")
                     .into(imageView)
 
-                //holder.categoryIconsContainer.addView(imageView)
+                holder.categoryIconsContainer.addView(imageView)
+            }
+        }
+
+        sizeInDp = 40 // Tamaño en dp
+        sizeInPx = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, sizeInDp.toFloat(), holder.view.resources.displayMetrics).toInt()
+
+        members = listOf("/api/images/user/638512277351423182.jpg", "/api/images/user/default_image.jpg", "/api/images/user/638513348400864796.jpg")
+        Glide.with(holder.view.context)
+            .load("${AppConstants.BASE_API_URL}/api/images/user/638513043795905625.jpeg")
+            .into(holder.profileImageCreator)
+        if (members?.isNotEmpty() == true) {
+            for (member in members!!) {
+                val imageView = ShapeableImageView(holder.view.context).apply {
+                    scaleType = ImageView.ScaleType.CENTER_CROP
+                    shapeAppearanceModel = ShapeAppearanceModel.builder(
+                        context,
+                        0,
+                        R.style.circular
+                    ).build()
+                }
+
+                val layoutParams = LinearLayout.LayoutParams(sizeInPx, sizeInPx)
+                layoutParams.setMargins(0, 0, -10, 0)
+                imageView.layoutParams = layoutParams
+                Glide.with(holder.view.context)
+                    .load("${AppConstants.BASE_API_URL}${member}")
+                    .into(imageView)
+
+                holder.peopleProfilesContainer.addView(imageView)
             }
         }
 
